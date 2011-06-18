f45cb9e1c2c6ccb59560df6bef10db7f9ac15a3d
Florin Boariu <florin@rootshell.ro>
2011-06-18 16:08:00 +0200

    deleted tree widget -- will be removed

--- paul/browser/browserwindow.py
+++ paul/browser/browserwindow.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file '../../ui/browserwindow.ui'
 #
-# Created: Fri Jun 17 14:34:09 2011
-#      by: The PyQt User Interface Compiler (pyuic) 3.17.4
+# Created: Sa Jun 18 16:07:22 2011
+#      by: The PyQt User Interface Compiler (pyuic) 3.18.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -20,16 +20,13 @@ class BrowserWindow(QMainWindow):
         if not name:
             self.setName("BrowserWindow")
 
+        self.setSizePolicy(QSizePolicy(QSizePolicy.Preferred,QSizePolicy.Preferred,0,0,self.sizePolicy().hasHeightForWidth()))
 
         self.setCentralWidget(QWidget(self,"qt_central_widget"))
 
-        self.Dirlist = QListView(self.centralWidget(),"Dirlist")
-        self.Dirlist.addColumn(self.__tr("Column 1"))
-        self.Dirlist.setGeometry(QRect(11,11,150,420))
-
         self.Plotspace = MatplotlibWidget(self.centralWidget(),"Plotspace")
-        self.Plotspace.setGeometry(QRect(169,11,400,418))
-        self.Plotspace.setSizePolicy(QSizePolicy(QSizePolicy.Preferred,QSizePolicy.Preferred,0,0,self.Plotspace.sizePolicy().hasHeightForWidth()))
+        self.Plotspace.setGeometry(QRect(10,10,560,420))
+        self.Plotspace.setSizePolicy(QSizePolicy(QSizePolicy.Maximum,QSizePolicy.Maximum,0,0,self.Plotspace.sizePolicy().hasHeightForWidth()))
         self.Plotspace.setMinimumSize(QSize(50,50))
 
 
@@ -42,11 +39,6 @@ class BrowserWindow(QMainWindow):
 
     def languageChange(self):
         self.setCaption(self.__tr("Paul Browser"))
-        self.Dirlist.header().setLabel(0,self.__tr("Column 1"))
-        self.Dirlist.clear()
-        item = QListViewItem(self.Dirlist,None)
-        item.setText(0,self.__tr("New Item"))
-
 
 
     def __tr(self,s,c = None):
--- paul/browser/mplwidget.py
+++ paul/browser/mplwidget.py
@@ -29,7 +29,8 @@ class MatplotlibWidget(FigureCanvas):
         self.axes.hold(False)
 
         self.compute_initial_figure()
-        self.plot("/home/florin/local/analysis/uru2si2/2010-zpoint/jul2010.uxp-dir/jul10_urs11/t10k/jul10_urs11_09gif.ibw")
+        #self.plot("/home/florin/local/analysis/uru2si2/2010-zpoint/jul2010.uxp-dir/jul10_urs11/t10k/jul10_urs11_09gif.ibw")
+        self.plot (sys.argv[1])
         
         FigureCanvas.__init__(self, self.fig)
         self.reparent(parent, QPoint(0, 0))
@@ -64,7 +65,7 @@ class MatplotlibWidget(FigureCanvas):
             #        This means that our scale comes out upside-down, and possibly
             #        also left-right inverted. Need to fix than in igor.loadibw() !
             self.axes.imshow(data, aspect='auto',
-                             extent=[dim1_start, dim1_end,
-                                     dim2_start, dim2_end])
+                             extent=[min(dim1_start,dim1_end), max(dim1_start,dim1_end),
+                                     min(dim2_start,dim2_end), max(dim2_start,dim2_end)])
         else:
             print "MatplotWidget::plot: not implemented for dim > 2"
