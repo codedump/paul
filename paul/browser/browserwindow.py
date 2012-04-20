0af305b774a60c5199208cb3e440b951a2bbaef8
Florin Boariu <florin.bessy@rootshell.ro>
2012-04-20 15:00:25 +0200

    (annoying model games...)

--- paul/browser/browserwindow.py
+++ paul/browser/browserwindow.py
@@ -5,9 +5,27 @@ class BrowserWindow (QtGui.QMainWindow, Ui_BrowserWindow):
     def __init__ (self):
         QtGui.QMainWindow.__init__(self)
         Ui_BrowserWindow.__init__(self)
+
         self.setupUi (self)
-        
+
+        # model for the file system (dir tree)
         self.fileSys = QtGui.QFileSystemModel()
-        self.fileSys.setRootPath(QtCore.QDir.currentPath())
-        
+        self.fileSys.setRootPath("~/")
+
+        # model for ibw files in the 2nd list box
+        self.fileIbw = QtGui.QFileSystemModel()
+        self.fileIbw.setFilter (QtCore.QDir.Files)
+        self.fileIbw.setNameFilters ("*.ibw")
+
         self.fileTree.setModel (self.fileSys)
+        self.fileList.setModel (self.fileIbw)
+        self.fileTree.setColumnWidth (0, 200)
+
+        self.fileTree.activated.connect(self.dirSelected)
+
+    # called when user selected an entry from the dir list
+    @QtCore.pyqtSlot('QModelIndex')
+    def dirSelected (self, index):
+        print "selected: %s" % self.fileSys.filePath(index)
+        self.fileIbw.setRootPath (self.fileSys.filePath(index))
+        
