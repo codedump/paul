fcf7401cdaec6bc83bed6770109cf4cce4890099
Florin Boariu <florin.bessy@rootshell.ro>
2012-06-15 14:10:56 +0200

    qt plotscript / color scale adjuster

--- paul/test/plotscript2.pp
+++ paul/test/plotscript2.pp
@@ -2,20 +2,72 @@
 
 import logging
 log = logging.getLogger (__name__)
-set_win = None
 
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
+
+class UiElements:
+    pass
+
+class PlotElements:
+    pass
+
+GUI = UiElements()
+PLOT = PlotElements()
+
+@QtCore.pyqtSlot('double')
+def colValueChanged(i):
+    ''' this one is called when the spin box color value changed '''
+    global GUI, PLOT
+
+    if not hasattr(PLOT, 'waves') or len(PLOT.waves) == 0:
+        return
+
+    data = PLOT.waves[0]
+    if not len(PLOT.canvas.axes.images) > 0:
+        return
+    img = PLOT.canvas.axes.images[0]
+
+    dmin = data.min()
+    dmax = data.max()
+
+    col_min = GUI.col_min.value() * (dmax-dmin) + dmin
+    col_max = GUI.col_max.value() * (dmax-dmin) + dmin
+    log.debug ("Color scale values: \t%f, %f (%f, %f)" %
+               (col_min, col_max, GUI.col_min.value(), GUI.col_max.value()))
+    img.set_clim (col_min, col_max)
+    PLOT.canvas.draw()
+    
 
 def init(canvas):
     log.debug ("INIT")
-    global set_win
-    set_win = QtGui.QLabel ("Look ma', I can create Qt Widgets!")
+    global GUI
+    GUI.win = QtGui.QWidget()
+    GUI.win.setWindowTitle ("Volatile Plot Settings")
+    GUI.layout = QtGui.QGridLayout(GUI.win)
+    GUI.win.setLayout (GUI.layout)
+
+    GUI.col_min = QtGui.QDoubleSpinBox (GUI.win)
+    GUI.col_max = QtGui.QDoubleSpinBox (GUI.win)
+    GUI.col_max.setValue (1)
+    row = 0
+    for w in GUI.col_min, GUI.col_max:
+        w.valueChanged.connect(colValueChanged)
+        w.setSingleStep (0.01)
+        GUI.layout.addWidget (w, row, 1)
+        row += 1
+
+
+    GUI.layout.addWidget (QtGui.QLabel ("Color min:", GUI.win), 0, 0)
+    GUI.layout.addWidget (GUI.col_min, 0, 1)
+    GUI.layout.addWidget (QtGui.QLabel ("Color max:", GUI.win), 1, 0)
+    GUI.layout.addWidget (GUI.col_max, 1, 1)
+
     return
 
 def exit(canvas):
     log.debug ("EXIT")
-    global set_win
-    del set_win
+    global GUI
+    del GUI
     return
 
 def decorate(can, wav):
@@ -28,10 +80,10 @@ def decorate(can, wav):
     if not len(wav) > 0:
         return
 
-    global set_win
-    set_win.show()
-
-    data = wav[0]
+    global GUI, PLOT
+    GUI.win.show()
+    PLOT.canvas = can
+    PLOT.waves = wav
 
     can.axes.set_xlabel (r'$k_{||}$ (deg.)')
     can.axes.set_ylabel (r'E$_{total}$ (meV)')
@@ -39,20 +91,5 @@ def decorate(can, wav):
     yax = can.axes.get_ylim()
     can.axes.set_ylim (min(yax), max(yax))
 
-    log.debug ("Image values: \t\t%f, %f" % (data.min(), data.max()))
-    dmin = data.min()
-    dmax = data.max()
-
-    if not len(can.axes.images) > 0:
-        return
-
-    img = can.axes.images[0]
-
-    col_min = 0.00 * (dmax-dmin) + dmin
-    col_max = 0.59 * (dmax-dmin) + dmin
-    log.debug ("Color scale values: \t%f, %f" % (col_min, col_max))
-    
-    
-    img.set_clim (col_min, col_max)
     #can.fig.colorbar(img)
     
