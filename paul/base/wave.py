wave: added object properties for easy axes coordinate computation arpes: added physically correct (H-matrix diagonalization) algorithm for N-band hybridization, in arbitrary number of dimensions :-)

paul/base/wave.py
@@ -56,7 +56,7 @@ class AxisInfo(object):
     So... need to rethink this. Here's one possible sollution:
        . Implement AxisInfo as a subclass of dict()
        . Insert default keys 'offset', 'delta' and 'units'
-       . Make one non-bound method ax(), which can be applied
+       . Make one non-bound method dim(), which can be applied
          to any object (not only to waves) and which would
          return either the corresponding AxisInfo() object
          in case of a Wave, or a sane default (offset=0, delta=1)
@@ -407,8 +407,8 @@ class Wave(ndarray):
         '''
         Sets the axis scaling using delta / offset parameters
         '''
-        self.ax(aindex).offset = offset
-        self.ax(aindex).delta = delta
+        self.dim[aindex].offset = offset
+        self.dim[aindex].delta = delta
 
     
     def setLimits (self, aindex, left, right):
@@ -417,8 +417,8 @@ class Wave(ndarray):
         (rather left/right). depending on which limit
         is the greater, "delta" might also end up negative!
         '''
-        self.ax(aindex).offset = left
-        self.ax(aindex).delta = (right-left) / self.shape[aindex]
+        self.dim[aindex].offset = left
+        self.dim[aindex].delta = (right-left) / self.shape[aindex]
 
 
     @property
@@ -429,13 +429,35 @@ class Wave(ndarray):
         return self.info['axes']
 
 
-    def ax(self, aindex):
-        '''
-        Returns the axis information for the specified axis.
-        '''
-        return self.dim[aindex]
+        # OBSOLETE: use self.dim[] instead!
+    #def ax(self, aindex):    
+        #'''
+        #Returns the axis information for the specified axis.
+        #'''
+        #return self.dim[aindex]
 
+    @property
+    def ax(self):
+        '''
+        Returns a list containing 1-dim. arrays with the axes values for each dimension.
+        '''
+        return [d.range for d in self.dim]
 
+    
+    @property
+    def axn(self):
+        '''
+        Returns a list containing N-dim. arrays with the axes values for each
+        dimension. The arrays are created using numpy.broadcast_arrays() on Wave.ax.
+        '''
+        dno = len(self.shape)
+        if dno == 1:
+            return self.ax
+        indices    = [ tuple([None]*(i) + [slice(None)] + [None]*(dno-i-1)) for i in range(dno) ]
+        ranges_nd  = [ d.range[i] for d,i in zip(self.dim, indices) ]
+        return np.broadcast_arrays (*ranges_nd)
+
+        
     @property
     def imlim(self):
         '''
@@ -443,6 +465,7 @@ class Wave(ndarray):
         '''
         return (self.dim[1].offset, self.dim[1].end, self.dim[0].end, self.dim[0].offset)
 
+        
     @property
     def lim(self):
         '''
@@ -568,12 +591,12 @@ class Wave(ndarray):
             new_axi = AxisInfo(parent)
 
             if isinstance(index, slice):
-                old_axi = source.ax(i)
+                old_axi = source.dim[i]
                 i_start = index.start
                 i_stop  = index.stop
                 i_step  = index.step
                 if i_start is not None:
-                    new_axi.offset = source.ax(i).i2x(i_start)
+                    new_axi.offset = source.dim[i].i2x(i_start)
                 else:
                     new_axi.offset = old_axi.offset
                 if i_step is not None:
@@ -586,10 +609,10 @@ class Wave(ndarray):
                 i += 1
 
             elif hasattr(index, "__iter__"):
-                old_axi = source.ax(i)
+                old_axi = source.dim[i]
                 a = array(index).min()
-                new_axi.offset = source.ax(i).i2x(a.min())
-                new_axi.delta = (source.ax(i).i2x(a.max())-source.ax(i).i2x(a.min()))/float(len(a))
+                new_axi.offset = source.dim[i].i2x(a.min())
+                new_axi.delta = (source.dim[i].i2x(a.max())-source.dim[i].i2x(a.min()))/float(len(a))
                 new_axi.units = old_axi.units
                 new_info.append(new_axi)
                 #print "i: new info for axis", i, new_axi
@@ -607,7 +630,7 @@ class Wave(ndarray):
                 i += 1
 
         while i < len(source.info['axes']):
-            new_info.append(AxisInfo(parent=parent, copy_from=source.ax(i)))
+            new_info.append(AxisInfo(parent=parent, copy_from=source.dim[i]))
             #print "c: new info for axis", i, self.ax(i)
             i += 1
 
@@ -1032,11 +1055,11 @@ class Wave(ndarray):
                     if len(ind) == 3:
                         i_step = ind[2]
                 if i_start is not None:
-                    i_start = self.ax(axis_i).x2i(i_start)
+                    i_start = self.dim[axis_i].x2i(i_start)
                 if i_stop is not None:
-                    i_stop = self.ax(axis_i).x2i(i_stop)
+                    i_stop = self.dim[axis_i].x2i(i_stop)
                 if i_step is not None:
-                    i_step /= self.ax(axis_i).delta
+                    i_step /= self.dim[axis_i].delta
 
                 # interpolation recommendation
                 if ((i_start is not None) and (abs(i_start-round(i_start)) > 1e-10)):
@@ -1062,7 +1085,7 @@ class Wave(ndarray):
                 #
                 new_ind = []
                 for item in ind:
-                    new_ind.append(self.ax(axis_i).i2x(item))
+                    new_ind.append(self.dim[axis_i].i2x(item))
 
                 # recommended interpolation
                 if recmd_intrp in (None, 'lim'):
@@ -1087,7 +1110,7 @@ class Wave(ndarray):
                 #
                 # default: ind is probably a simple float()-able number
                 #
-                nr = self.ax(axis_i).x2i(ind)
+                nr = self.dim[axis_i].x2i(ind)
                 if abs(nr-round(nr)) > 1e-10:
                     if recmd_intrp not in (True, False):
                         recmd_intrp = 'lim'
@@ -1151,7 +1174,7 @@ class Wave(ndarray):
         i1 = ndarray([len(self.shape)], dtype=int)
         i2 = ndarray([len(self.shape)], dtype=int)
         for v, ai in zip(vals, range(len(self.shape))):
-            ax = self.ax(ai)
+            ax = self.dim[ai]
             ii[ai] = (ax.x2i(v))
             i1[ai] = floor(ii[ai])
             i2[ai] = i1[ai]+1
@@ -1669,7 +1692,7 @@ def _test_index_call(*call_i, **kwargs):
     for i in range(a2.ndim):
         a2.dim[i].delta   = 0.1
         a2.dim[i].offset -= (0.5 * a2.dim[i].end)
-        verbose and pprint (str(a2.ax(i)))
+        verbose and pprint (str(a2.dim[i]))
 
     if lim_i  == True:
         lim_i  = call_i

paul/toolbox/arpes.py
@@ -8,6 +8,7 @@ import scipy as sp
 import scipy.interpolate as spi
 import scipy.ndimage.interpolation as spni
 import scipy.ndimage as spimg
+import scipy.linalg as splin
 from pprint import pprint
 import paul.base.wave as wave
 from paul.toolbox.atrix import ncomp
@@ -137,15 +138,17 @@ def _hybridize_n2n (wlist, V=0.0, count=1):
     times (default is 1). This is intended for multi-band hybridization,
     where one pass may not be enough.
     '''
-    # if only one value is specified, construct a coupling matrix out of it
-    if type(V) == float:
-        V = np.matrix([[V for i in wlist] for j in wlist]) / 2
+    
+    # Construct V-matrix, if needed.
+    if not hasattr(V, 'shape') or V.shape != (len(wlist), len(wlist)):
+        V = np.matrix([[float(V) for i in wlist] for j in wlist])
 
-    V -= np.diag(np.diag(V))
-
-    V2  = V + V.T - 2*np.diag(np.diag(V))      # make sure matrix is symmetric
-    V2 -= np.diag(np.diag(V))                  # remove diagonal elements
-
+    # Symmetrize V-matrix, remove diagonal, average over 
+    # elements that are non-zero in both triangles.
+    V2  = (V+V.T - 2*np.diag(np.diag(V)))  /  ((V!=0).astype(float) + 
+                                               (V!=0).astype(float).T + 
+                                               (V==0).astype(float))
+    
     ''' This is what we'll be operating on'''
     hlist = list(wlist)
     
@@ -186,16 +189,15 @@ def _hybridize_n2n (wlist, V=0.0, count=1):
             for j in range(len(hlist)):
                 if i == j:
                     continue
-                h1 = 0.5 * (hlist[i] + hlist[j]) + \
-                    np.sqrt( (0.5*(hlist[j]-hlist[i]))**2 + V2[i,j]**2 )
-                h2 = 0.5 * (hlist[i] + hlist[j]) - \
-                    np.sqrt( (0.5*(hlist[j]-hlist[i]))**2 + V2[i,j]**2 )
+                h1 = 0.5*(hlist[i]+hlist[j]) + np.sqrt( (0.5*(hlist[j]-hlist[i]))**2 + V2[i,j]**2 )
+                h2 = 0.5*(hlist[i]+hlist[j]) - np.sqrt( (0.5*(hlist[j]-hlist[i]))**2 + V2[i,j]**2 )
                 hlist[i] = h1
                 hlist[j] = h2
 
     return hlist
+    
 
-def _hybridize_hmatrix (wlist, V=0.0):
+def _hybridize_hmatrix (wlist, V=0.0, count='(ignored)'):
     '''
     Hybridizes bands from *wlist* using the coupling matrix *V*.
     *V* is a NxN matrix, where N = len(wlist).
@@ -216,10 +218,61 @@ def _hybridize_hmatrix (wlist, V=0.0):
     where vxy = sqrt (Vxy), ei(k) are the non-hybridized bands,
     and the diagonal elements of the diagonalized matrix hi(k)
     will be the hybridized bands.
-    '''
-    pass
 
-hybridize = _hybridize_n2n
+    Parameters:
+      - `wlist`: List of (non-hybridized) N-dim. bands. They are assumed to all have
+      the same resolution and axis values. If they are waves, then the axes
+      ranges of the first wave are used
+      
+      - kcoord`: (optional) list of (N-1)-dimensional axis coordinates, one for each axis.
+      _Each_ element of the list is an N-dimensional array (which could be generated,
+      for example, using numpy.broadcast_arays() on the original axes ranges, or
+      returned by the Wave.axn property.)
+
+      - V: the hybridization potential, specified either as a NxN matrix or a floating
+      point value. If it's a float, then a matrix is constructed with V/2 at all
+      positions. If it's a matrix, the matrix is symmetrized by duplicating non-zero
+      elements from one trinagle into the zeros of the other triangle, or by averaging
+      at positions where non-zero elements are available in both triangles.
+      Please not that zero elements *must* be a truly NULL -- small floating
+      point numbers will _not_ be recognized as zero!
+    '''
+
+    # build V-matrix, if necessary
+    if not hasattr(V, 'shape') or V.shape != (len(wlist), len(wlist)):
+        V = np.matrix([[float(V) for i in wlist] for j in wlist])
+
+    # Symmetrize V-matrix, remove diagonal, average over 
+    # elements that are non-zero in both triangles.
+    V2  = (V+V.T - 2*np.diag(np.diag(V)))  /  ((V!=0).astype(float)   + 
+                                               (V!=0).astype(float).T + 
+                                               (V==0).astype(float))
+    
+    # flatten bands and zip them element-wise together
+    ebands = zip(*[b.flat for b in wlist])
+
+    # This is where all the magic happens ;-)
+    #
+    # At this points, we have a 2D matrix (ebands) containing
+    # all original band diagonals (dimension 0), at all k-values
+    # flattened (dimension 1). The only thing we need to do is add
+    # them to V2 to build a 'perturbed' H-matrix and diagonalize it.
+    # For hybridzed bands, eigenvalues need to be re-sorted to avoid
+    # band crossings.
+    hbands = [ np.sort(np.real(splin.eig (V2 + np.diag(Ek), left=False, right=False)))
+               for Ek in ebands ]
+
+    # Done, now we basically only reformat the output bands to
+    # match the data type and vector layout of the input data.
+    _hlist = [ np.reshape(_h, wlist[0].shape) for _h in  np.array(hbands).T ]
+    hlist  = [ np.empty_like(w) for w in wlist ]
+    for h, _h in zip(hlist, _hlist):
+        h[...] = _h[...]
+
+    return hlist
+    
+
+hybridize = _hybridize_hmatrix
 
 
 #